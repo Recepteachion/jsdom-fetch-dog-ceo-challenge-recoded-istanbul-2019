d0f67fc62153c09ac008b4d4ec886d97a1934f67
Recep Öztürk <turkishteacher23@gmail.com>
2020-02-03 19:30:25 +0000

    Done.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,17 @@
 console.log('%c HI', 'color: firebrick')
+const imgUrl = "https://dog.ceo/api/breeds/image/random/4";
+const imgCon = document.getElementById("dog-image-container");
+let newIm = document.createElement("img");
+
+imgCon.appendChild(newIm);
+document.querySelector("img").id ="image";
+
+
+fetch(imgUrl).then(function(response){
+	return response.json()
+	.then(function(json){
+		document.getElementById("image").src=json.message[0];
+		
+
+	})
+})
